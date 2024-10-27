ce386ccdd5344c6e848461bd2d6f5e41df23c7a5
Kleyton Félix <81056671+KleytonFelix@users.noreply.github.com>
2024-10-27 20:24:57 -0300

    Update ola.py

--- ola.py
+++ ola.py
@@ -1,3 +1,15 @@
 import os
-print('Olá, mundo!')
-os.system("pause")
+# Aqui eu importei o módulo "os" do Windows, para usar a função "pause" do sistema operacional.
+
+print('Olá, mundo!') # função print para gerar a sáida e mostrar a mensagem que está entre aspas simples
+
+os.system("pause") # O python se comunica com sistema operacional Windows para utilizar o comando pause
+
+'''
+Foi uma alternativa bacana que encontrei para que a janela do terminal não fechasse logo após executar 
+o arquivo. 
+Aqui o comando pause é colocado entre aspas porque estamos passando o comando pause como uma string para a
+função os.system()
+Se não fosse colocado entre aspas o Python iria interpretar como uma variável ou função ao invés de um
+comando do sistema
+'''
