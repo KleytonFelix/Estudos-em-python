5f43b919d5e962964609bf3e0cbb0bf5e9c4dce1
Kleyton Félix <81056671+KleytonFelix@users.noreply.github.com>
2024-11-17 15:18:17 -0300

    Create RAATINHOO.py

---
+++ RAATINHOO.py
@@ -0,0 +1,12 @@
+print("Digite qualquer coisa que irei repetir, igual aquela sua irmã chata, mas como sou um robô, eu tenho um bip no final")
+print("Para sair do programa, peça arrego")
+
+while True:
+    texto = input("")
+    print(texto + " ,bip")
+    if texto == "arrego":
+        break
+
+print("Arregou não foiii!??? IIIIIIIRRRAA, RRATINHOOOOOO,")
+print("Saindo do programa")
+input("Press Enter to exit")
